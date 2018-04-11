8bbc975bae48317908ddb6aaf7e7f26a9e815358
Miguel <miguelreybad@gmail.com>
2018-04-11 11:19:23 -0500

    Creando nueva clase

---
+++ src/main/java/mx/itam/adsi/ejemplo/Aritmetico.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+public class Aritmetico{
+
+	public Aritmetico(){
+
+	}
+
+	public int min(int a, int b){
+		return Math.min(a,b);
+	}
+
+	public int abs(int a){
+		return Math.abs(a);
+	}
+}
